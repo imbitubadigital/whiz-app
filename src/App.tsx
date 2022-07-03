237c8b000fe9daec0fccc4aa2cdb6888ce2e61ca
Antonio Fernandes <imbitubadigital@gmail.com>
2022-07-03 09:16:36 -0300

    feat: install dependences

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Teste} from './components/Teste';
+import {Teste} from '@components/Teste';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your apps!</Text>
+      <Text>Open up App.js to start working on your appsXXX!</Text>
       <Teste />
       <StatusBar style="auto" />
     </View>
--- src/components/Teste/index.tsx
+++ src/components/Teste/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import About from '../../assets/about.svg';
 import * as S from './styles';
 
 export function Teste() {
   return (
-    <S.Container>
-      <S.Title>Teste</S.Title>
-    </S.Container>
+    <>
+      <S.Container>
+        <S.Title>Teste</S.Title>
+      </S.Container>
+      <About />
+    </>
   );
 }
